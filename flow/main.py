6ce0278f32107cf02a9c0c2d8a73b8079cbf73da
tfe <Takfes@users.noreply.github.com>
2025-01-09 00:11:25 +0200

    add plotting functions + mip optimization + walk through simulation

--- flow/main.py
+++ flow/main.py
@@ -1,12 +1,19 @@
-from itertools import count
-from operator import index
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 from euroleague_api.game_stats import GameStats
 
 from elfantasy.config import DATA_DIR
-from elfantasy.functions import get_euroleague_data, standings_from_games, tidy_euroleague_data, tidy_games_data
+from elfantasy.functions import (
+    get_euroleague_data,
+    optimize_team,
+    plot_stats_boxes,
+    plot_stats_lines,
+    standings_from_games,
+    tidy_euroleague_data,
+    tidy_games_data,
+)
 
 competition_code = "E"
 season = 2024
@@ -18,6 +25,92 @@ standings = standings_from_games(games)
 
 df_raw = get_euroleague_data()
 df = tidy_euroleague_data(df_raw)
+
+"""
+# ==============================================================
+# Simulation - Predictions
+# ==============================================================
+"""
+
+# if were to use the previous week's valuation as a predictor
+df_predict = df.copy()
+df_predict["valuation_lag_1"] = (
+    df_predict.sort_values(by=["week"], ascending=True).groupby(["slug"])["valuation"].shift()
+)
+
+# if were to use the rolling average of the previous 3 weeks as a predictor
+df_predict["valuation_roll_3"] = (
+    df_predict.sort_values(by=["week"], ascending=True)
+    .groupby(["slug"])["valuation"]
+    .rolling(3, closed="left")
+    .mean()
+    .reset_index(0, drop=True)
+)
+
+# Calculate errors
+df_predict["error_lag_1"] = df_predict["valuation"] - df_predict["valuation_lag_1"]
+df_predict["error_roll_3"] = df_predict["valuation"] - df_predict["valuation_roll_3"]
+
+# Review
+df_predict[["error_lag_1", "error_roll_3"]].describe()
+
+# Initialize objects to store solutions
+solutions_optimal = {}
+solutions_lag_1 = {}
+solutions_roll_3 = {}
+
+# Optimize teams for each week using different predictors
+for w in range(4, df_predict.week.max().item() + 1):
+    print(f"Optimizing week {w}")
+    dfw = df_predict[df_predict["week"] == w].reset_index(drop=True).copy()
+    # Optimal
+    team, obj, budget = optimize_team(dfw, value_col="valuation", budget=100)
+    solutions_optimal[w] = {"team": team, "obj": obj, "budget": budget}
+    # Lag 1
+    team, obj, budget = optimize_team(dfw, value_col="valuation_lag_1", budget=100)
+    solutions_lag_1[w] = {"team": team, "obj": obj, "budget": budget}
+    # Roll 3
+    team, obj, budget = optimize_team(dfw, value_col="valuation_roll_3", budget=100)
+    solutions_roll_3[w] = {"team": team, "obj": obj, "budget": budget}
+
+# Collect results
+simres = pd.concat(
+    [
+        pd.DataFrame(solutions_optimal).T.rename(columns=lambda x: f"{x}_optimal"),
+        pd.DataFrame(solutions_lag_1).T.rename(columns=lambda x: f"{x}_lag_1"),
+        pd.DataFrame(solutions_roll_3).T.rename(columns=lambda x: f"{x}_roll_3"),
+    ],
+    axis=1,
+)[
+    [
+        "obj_optimal",
+        "obj_lag_1",
+        "obj_roll_3",
+        "budget_lag_1",
+        "budget_optimal",
+        "budget_roll_3",
+        "team_optimal",
+        "team_lag_1",
+        "team_roll_3",
+    ]
+]
+
+# Calculate differences
+simres["obj_opt_vs_lag_1"] = (simres["obj_optimal"] - simres["obj_lag_1"]).astype(float)
+simres["obj_opt_vs_roll_3"] = (simres["obj_optimal"] - simres["obj_roll_3"]).astype(float)
+
+# Review
+simres[["obj_opt_vs_lag_1", "obj_opt_vs_roll_3"]].describe()
+
+"""
+# ==============================================================
+# Data Exploration
+# ==============================================================
+"""
+
+plot_stats_boxes(df, position="G", criterion="greedy", category="high", stats_agg_func="std")
+
+plot_stats_lines(df, "theo-maledon")
 
 player_averages = (
     # df.groupby(["slug", "position", "team_name"], as_index=False)
@@ -36,8 +129,6 @@ player_averages = (
     .sort_values("value_for_credit", ascending=False)
     .fillna(0)
 )
-
-player_averages.to_clipboard(index=False)
 
 team_averages = (
     df.groupby(["team_name", "position"], as_index=False)
--- src/elfantasy/functions.py
+++ src/elfantasy/functions.py
@@ -1,6 +1,129 @@
+import time
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import pyomo.environ as pyo
 import requests
+import seaborn as sns
+
+
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        elapsed_time = end_time - start_time
+        print(f"Function '{func.__name__}' executed in {elapsed_time:.4f} seconds")
+        return result
+
+    return wrapper
+
+
+@timeit
+def optimize_team(data, value_col, budget=100, guards=4, forwards=4, centers=2):
+    df = data.copy()
+    max_players = guards + forwards + centers
+
+    # prepare data
+    slugs = df["slug"].unique()
+    positions = df["position"].unique()
+    posg = df[df.position == "G"]["slug"].unique()
+    posf = df[df.position == "F"]["slug"].unique()
+    posc = df[df.position == "C"]["slug"].unique()
+    values = df[["slug", value_col]].fillna(0).set_index("slug").to_dict()[value_col]
+    costs = df[["slug", "cr"]].fillna(0).set_index("slug").to_dict()["cr"]
+
+    # Create a model
+    model = pyo.ConcreteModel()
+
+    # Define the sets
+    model.P = pyo.Set(initialize=slugs)
+    model.pos = pyo.Set(initialize=positions)
+    model.posg = pyo.Set(initialize=posg)
+    model.posf = pyo.Set(initialize=posf)
+    model.posc = pyo.Set(initialize=posc)
+
+    # Define parameters
+    model.v = pyo.Param(model.P, initialize=values)
+    model.c = pyo.Param(model.P, initialize=costs)
+    model.max_players = pyo.Param(initialize=max_players)
+    model.guards = pyo.Param(initialize=guards)
+    model.forwards = pyo.Param(initialize=forwards)
+    model.centers = pyo.Param(initialize=centers)
+    model.budget = pyo.Param(initialize=budget)
+
+    # Define decision variables
+    model.x = pyo.Var(model.P, within=pyo.Binary)
+
+    # Define constraints
+    def cstr_max_players(model):
+        return sum(model.x[p] for p in model.P) == model.max_players
+
+    def cstr_guards(model):
+        return sum(model.x[p] for p in model.posg) == model.guards
+
+    def cstr_forwards(model):
+        return sum(model.x[p] for p in model.posf) == model.forwards
+
+    def cstr_centers(model):
+        return sum(model.x[p] for p in model.posc) == model.centers
+
+    def cstr_budget(model):
+        return sum(model.x[p] * model.c[p] for p in model.P) <= model.budget
+
+    model.cstr_max_players = pyo.Constraint(rule=cstr_max_players)
+    model.cstr_guards = pyo.Constraint(rule=cstr_guards)
+    model.cstr_forwards = pyo.Constraint(rule=cstr_forwards)
+    model.cstr_centers = pyo.Constraint(rule=cstr_centers)
+    model.cstr_budget = pyo.Constraint(rule=cstr_budget)
+
+    # Define objective function
+    def obj(model):
+        return sum(model.x[p] * model.v[p] for p in model.P)
+
+    model.obj = pyo.Objective(rule=obj, sense=pyo.maximize)
+
+    # solver options ------------------------------------------------
+    solver = pyo.SolverFactory("appsi_highs")
+
+    solver.options["tee"] = False
+    solver.options["parallel"] = "on"
+    solver.options["time_limit"] = 3600 / 2  # 30 minutes time limit
+    solver.options["presolve"] = "on"
+    solver.options["mip_rel_gap"] = 0.01  # 1% relative gap
+    solver.options["simplex_strategy"] = 1  # Dual simplex
+    solver.options["simplex_max_concurrency"] = 8  # Max concurrency
+    solver.options["mip_min_logging_interval"] = 10  # Log every 10 seconds
+    solver.options["mip_heuristic_effort"] = 0.2  # Increase heuristic effort
+    solver.options["log_file"] = (
+        "highs.log"  # Sometimes HiGHS doesn't update the console as it solves, so write log file too
+    )
+    # solver options ------------------------------------------------
+
+    solver.solve(model, tee=False)
+
+    solution = {k: int(v > 0.55) for k, v in model.x.extract_values().items()}
+    df["solution"] = df["slug"].map(solution)
+    df1 = df[df.solution == 1]
+
+    # validation ------------------------------------------------
+    assert df1.shape[0] == max_players, f"More than {max_players} players selected"
+
+    assert df1[df1.position == "G"].slug.nunique() == guards, f"More than {guards} guards selected"
+
+    assert df1[df1.position == "F"].slug.nunique() == forwards, f"More than {forwards} forwards selected"
+
+    assert df1[df1.position == "C"].slug.nunique() == centers, f"More than {centers} centers selected"
+
+    assert df1.cr.sum().item() <= budget + 0.001, f"Budget exceeded: {df1.cr.sum().item()} > {budget}"
+
+    assert df1.groupby("team_code").size().max().item() <= 6, "More than 6 players from the same team selected"
+    # end validation --------------------------------------------
+
+    # objective_value = model.obj()
+    objective_value = df[(df.solution == 1)].valuation.sum().item()
+    return df1.slug.unique().tolist(), objective_value, df1.cr.sum().item()
 
 
 def get_euroleague_data(season_id=17, stats_type="avg"):
@@ -266,3 +389,108 @@ def standings_from_games(df, streak=(1, 3, 5)):
         column_order.append(colname)
 
     return standings[column_order].drop(columns=["LastGames"])
+
+
+def plot_stats_boxes(
+    df,
+    position,
+    criterion="cr",
+    category="high",
+    stats_agg_func="mean",
+    topx_players_presence=20,
+    stats_to_show=5,
+    max_players_to_show=8,
+):
+    infovars = ["slug", "team_name", "position", "team_code"]
+
+    variables = [
+        "fgm",
+        "tpm",
+        "ftm",
+        "ast",
+        "reb",
+        "stl",
+        "blk",
+        "blka",
+        "oreb",
+        "dreb",
+        "tov",
+        "pf",
+        "fouls_received",
+    ]
+
+    # select player with enough minutes and in specific position
+    df_position = (
+        df[df.position == position]
+        .loc[lambda x: x.slug.isin(x.groupby("slug")["min"].mean().nlargest(topx_players_presence).index)]
+        .assign(greedy=lambda x: x.valuation / x.cr)
+    )
+
+    # split into categories - low, medium, high
+    player_cuts = pd.qcut(
+        df_position.groupby("slug")[criterion].mean(), q=3, labels=["low", "medium", "high"]
+    ).to_dict()
+
+    df_filtered = df_position.loc[lambda x: x.slug.isin([x for x in player_cuts if player_cuts[x] == category])]
+
+    if df_filtered.slug.nunique() > 8:
+        df_filtered = df_filtered.loc[
+            lambda x: x.slug.isin(x.groupby("slug")[criterion].mean().nlargest(max_players_to_show).index)
+        ]
+
+    # select most relevant stats
+    variables_filtered = df_filtered[variables].agg(stats_agg_func).nlargest(stats_to_show).index
+
+    # melt the dataframe
+    df_long = df_filtered.melt(id_vars=infovars, value_vars=variables_filtered)
+
+    # Set the color palette
+    palette = sns.color_palette("Paired")
+
+    # Create the plot
+    sns.boxplot(data=df_long, x="variable", y="value", hue="slug", palette=palette)
+    plt.legend(bbox_to_anchor=(1.05, 1), loc="upper left", fontsize="small")
+    plt.show()
+
+
+def plot_stats_lines(df, slug, stats_agg_func="mean", stats_to_show=3):
+    df_player = df[df.slug == slug].copy()
+
+    variables = [
+        "fgm",
+        "tpm",
+        "ftm",
+        "ast",
+        "reb",
+        "stl",
+        "blk",
+        "blka",
+        "oreb",
+        "dreb",
+        "tov",
+        "pf",
+        "fouls_received",
+    ]
+
+    evalvars = ["valuation", "cr"]
+
+    # select most relevant stats
+    variables_filtered = df_player[variables].agg(stats_agg_func).nlargest(stats_to_show).index
+
+    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(12, 8))
+
+    for var in evalvars:
+        ax1.plot(df_player.week, df_player[var], label=var)
+    ax1.set_title("evaluation over time")
+    ax1.legend()
+
+    for var in variables_filtered:
+        ax2.plot(df_player.week, df_player[var], label=var)
+    ax2.set_title("statistics over time")
+    ax2.legend()
+
+    ax1.set_xticks(df_player.week)
+    ax1.set_xticklabels(df_player.week)
+    plt.xlabel("Time")
+    plt.tight_layout()
+    plt.show()
